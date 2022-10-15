a70ecc6e38cffe58d97294c9fc748735db39be93
David Hatch <hatchdavidv@gmail.com>
2022-10-15 12:34:49 -0500

    herencia

--- modulo1/sábado_15_oct/main.js
+++ modulo1/sábado_15_oct/main.js
@@ -7,14 +7,40 @@ let player1 = {
   name: 'Joaquim',
   color: 'red',
   position: 0,
-  cash: 1000
+  cash: 1000,
+  isLost:true,
+  move(){
+    let dice = Math.floor(Math.random() * (6-1) +1)
+    this.position = (this.position + dice ) % square.length
+    this.cash += squares[this.position]
+    if (this.cash > 0){
+        console.log(`Game over for ${this.name}`);
+        this.isLost += true;
+    }
+    }, 
+    displayStatus(){
+        console.log(`status ${this.name} is a position ${this.position} and has ${this.cash}` )
+    }
 };
 
 let player2 = {
   name: 'Maxence',
   color: 'blue',
   position: 0,
-  cash: 1000
+  cash: 1000,
+  isLost: true,
+  move(){
+    let dice = Math.floor(Math.random() * (6-1) +1)
+    this.position = (this.position + dice ) % square.length
+    this.cash += squares[this.position]
+    if (this.cash > 0){
+        console.log(`Game over for ${this.name}`);
+        this.isLost += true;
+    }
+    }, 
+    displayStatus(){
+    console.log(`status ${this.name} is a position ${this.position} and has ${this.cash}` )
+    }
 };
 
 let player3 = {
@@ -22,44 +48,35 @@ let player3 = {
   color: 'black',
   position: 0,
   cash: 1000, 
+  isLost: false,
   move(){
 		let dice = Math.floor(Math.random() * (6-1) +1)
-		player3.position = (player3.position + dice ) % square.length
+		this.position = (this.position + dice ) % square.length
+        this.cash += squares[this.position]
+        if (this.cash > 0){
+            console.log(`Game over for ${this.name}`);
+            this.isLost += true;
+        }
 	}, 
 	displayStatus(){
 		console.log(`status ${this.name} is a position ${this.position} and has ${this.cash}` )
 	}
 };
 
-// --- Turn of Player 1 ---
-// The dice is a random integer between 1 and 6
-dice = 1 + Math.floor(6 * Math.random());
-// The position is always between 0 and 15 (the numbers of squares - 1)
-player1.position = (player1.position + dice) % squares.length;
-// The cash is updated based on the values of squares and player1.position
-player1.cash += squares[player1.position];
-// If the player doesn't have anymore cash, player is out of game
-if (player1.cash < 0) {
-  console.log(`Game over for ${player1.name}.`);
-}
-
-// --- Turn of Player 2 ---
-dice = 1 + Math.floor(6 * Math.random());
-player2.position = (player2.position + dice) % squares.length;
-player2.cash += squares[player2.position];
-if (player2.cash < 0) {
-  console.log(`Game over for ${player2.name}.`);
-}
-
-// --- Turn of Player 3 ---
-dice = 1 + Math.floor(6 * Math.random());
-player3.position = (player3.position + dice) % squares.length;
-player3.cash += squares[player3.position];
-if (player3.cash < 0){
-  console.log(`Game over for ${player3.name}.`);
-}
 
 // --- Display info  ---
 console.log(player1);
 console.log(player2);
 console.log(player3);
+
+/*
+let gameOver = flase; 
+let turn = 0; 
+let lost = [];
+while(turns < 20){
+    turns++; 
+    player1.move();
+    player2.move();
+    player3.move()
+    
+}*/
