0f5abe64eff31d2f6f817a470672fa3cd7bf2f1c
David Hatch <hatchdavidv@gmail.com>
2022-11-14 21:22:10 -0600

    promises

--- modulo2/js-promises/src/index.js
+++ modulo2/js-promises/src/index.js
@@ -36,6 +36,7 @@ function getDirections(step, callback, errorCallback) {
     }, Math.floor(Math.random() * 1000))
 }
 
+/**Callback HEll 
 getDirections(0, ()=>{
     getDirections(1, ()=>{
         getDirections(2, ()=>{
@@ -46,3 +47,60 @@ getDirections(0, ()=>{
     })
 
 }, (error)=>{console.error(error)})
+*/
+
+function obtainDirections(step) {
+    return new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            console.log('P',directions[step]);
+            if(!directions[step]) reject('No hay más rutas')
+            else resolve()
+        }, Math.floor(Math.random()*3000))
+    })
+}
+
+/* obtainDirections(0).then(()=>{
+    console.log('Dentro del then')
+    return obtainDirections(1)
+}).then(()=>
+    obtainDirections(2)
+).then(()=>
+    obtainDirections(3)
+).catch((error)=>{
+    console.warn(error);
+}).finally(()=>{
+    console.log('Soy el final');
+})
+*/
+
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(()=>resolve('foo'), 10000)
+})
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(()=>resolve(12345), 2000)
+})
+const p3 = new Promise((resolve, reject) => {
+    setTimeout(()=>resolve({name:'Jose'}), 4000)
+})
+const p4 = new Promise((resolve, reject) => {
+    setTimeout(()=>resolve('Cualquier cosa'), 1000)
+})
+
+
+p1.then((response)=>{
+    console.log('la respuesta', response);
+}).catch(()=>{})
+p2.then((response)=>{
+    console.log('la respuesta', response);
+}).catch(()=>{})
+p3.then((response)=>{
+    console.log('la respuesta', response);
+}).catch(()=>{})
+p4.then((response)=>{
+    console.log('la respuesta', response);
+}).catch(()=>{})
+
+Promise.all([p1,p2,p3,p4])
+.then((res)=>{console.log('la res', res);})
+.catch(()=>{})
+//.finally(()=>{})
